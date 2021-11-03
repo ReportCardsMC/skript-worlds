caaf920117782e31a216b808651621a115f64900
Eliza <rileydatlord@gmail.com>
2021-11-03 00:14:13 -0700

    Register the addon correctly

--- src/main/java/me/github/reportcardsmc/skriptworlds/SkriptWorlds.java
+++ src/main/java/me/github/reportcardsmc/skriptworlds/SkriptWorlds.java
@@ -1,14 +1,19 @@
 package me.github.reportcardsmc.skriptworlds;
 
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptAddon;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 public final class SkriptWorlds extends JavaPlugin {
 
 
     public static MultiverseCore mvCore;
     public static SkriptWorlds instance;
+    public static SkriptAddon addon;
 
     @Override
     public void onEnable() {
@@ -19,6 +24,14 @@ public final class SkriptWorlds extends JavaPlugin {
             return;
         }
         mvCore = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+        getLogger().info("MV-Core found and loaded, enabling Skript Addon");
+        addon = Skript.registerAddon(this);
+        try {
+            addon.loadClasses("me.github.reportcardsmc.skriptworlds", "skript");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        getLogger().info("Addon has been enabled");
 
     }
 
