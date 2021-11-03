63c897b3e3abce2a4a05c8cc9e924c753567e88b
Eliza <rileydatlord@gmail.com>
2021-11-02 23:58:26 -0700

    Initial Commit

---
+++ src/main/java/me/github/reportcardsmc/skriptworlds/SkriptWorlds.java
@@ -0,0 +1,17 @@
+package me.github.reportcardsmc.skriptworlds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkriptWorlds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
