cb255f3e68993634f61badfc2c7b60296dd035ae
Eliza <rileydatlord@gmail.com>
2021-11-03 00:02:42 -0700

    Get MV Core class

--- src/main/java/me/github/reportcardsmc/skriptworlds/SkriptWorlds.java
+++ src/main/java/me/github/reportcardsmc/skriptworlds/SkriptWorlds.java
@@ -1,12 +1,24 @@
 package me.github.reportcardsmc.skriptworlds;
 
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkriptWorlds extends JavaPlugin {
 
+
+    public static MultiverseCore mvCore;
+    public static SkriptWorlds instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+        if (Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core") == null) {
+            getLogger().severe("Multiverse is not loaded.");
+            return;
+        }
+        mvCore = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
 
     }
 
